564c129564e9be12b4d1e6e505f453283446aa7f
Cisnoc Xie <502021156@qq.com>
2018-07-29 20:58:31 +0800

    manytomany

---
+++ src/main/java/com/oocl/jpapractice/manytomany/entities/Group.java
@@ -0,0 +1,9 @@
+package com.oocl.jpapractice.manytomany.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table(name = "group")
+@Entity
+public class Group {
+}
---
+++ src/main/java/com/oocl/jpapractice/manytomany/entities/User.java
@@ -0,0 +1,9 @@
+package com.oocl.jpapractice.manytomany.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table(name = "user")
+@Entity
+public class User {
+}
